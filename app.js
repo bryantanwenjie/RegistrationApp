323539f99750434b2e663e94fe67b35e7bea381e
Bryan <24001655@myrp.edu.sg>
2025-07-17 12:41:04 +0800

    db connect

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'C237_usersdb'
+    //host: 'localhost',
+    //user: 'root',
+    //password: '',
+    //database: 'C237_usersdb'
+    host: 'c237-all.mysql.database.azure.com',
+    port: 3306,
+    user: 'c237admin',
+    password: 'c2372025!',
+    database: 'c237_24001655'
 });
 
 db.connect((err) => {
